89fccb271933b6ea1d14ca2100da6109507ab8ca
AlejandroFloresM02 <madrizfa02@gmail.com>
2026-06-23 18:35:12 -0600

    feat: added command line input callback interface

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,26 @@
+import { createInterface } from "readline";
+
+export function startRepl() {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+  rl.prompt();
+
+  rl.on("line", async (input) => {
+    const words = cleanInput(input);
+    words.length === 0
+      ? rl.prompt()
+      : console.log(`Your command was: ${words[0]}`);
+    rl.prompt();
+  });
+}
+
 export function cleanInput(input: string): string[] {
-  return input.trim().toLocaleLowerCase().split(" ");
+  return input
+    .toLocaleLowerCase()
+    .trim()
+    .split(" ")
+    .filter((word) => word !== "");
 }
